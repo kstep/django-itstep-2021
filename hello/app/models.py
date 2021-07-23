e34d5a813c1fe496d0c23e06b7649a7a26ef2101
Konstantin Stepanov <me@kstep.me>
2021-07-23 16:49:31 +0300

    initial commit

---
+++ hello/app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(
+        max_length=200)
+    group = models.IntegerField()
+    birthday = models.DateField()
+
+
