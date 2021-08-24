96ba34432ab21043ae7a0fbe1c2d2240b4d8c899
Konstantin Stepanov <me@kstep.me>
2021-08-24 19:51:40 +0300

    remove hello view

--- hello/app/views.py
+++ hello/app/views.py
@@ -23,36 +23,6 @@ def show_students(request):
                   "show_students.html",
                   {'students': students})
 
-def hello(request: HttpRequest, a, b, c):
-    """
-    Solve square equation, requires
-    a, b, and c in query-string.
-
-    :param request:
-    :return:
-    """
-    try:
-        if a == 0:
-            x1 = -c / b
-            x2 = x1
-        else:
-            d = (b*b - 4*a*c) ** 0.5
-            x1 = (-b + d) / 2*a
-            x2 = (-b - d) / 2*a
-
-        return render(request, "hello.txt", {
-            'x1': x1,
-            'x2': x2,
-        })
-    except KeyError:
-        return HttpResponse("I need a, b, and c", status=400)
-    except ValueError:
-        return HttpResponse("I accept numbers only", status=400)
-    except ZeroDivisionError:
-        return HttpResponse("Both a and b can't be zero", status=400)
-
-
-
 
 def length(request: HttpRequest):
     if 'str' in request.GET:
