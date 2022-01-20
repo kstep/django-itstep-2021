74a7e1110f93377dce58331766846399efdba400
Konstantin Stepanov <me@kstep.me>
2022-01-20 18:57:19 +0300

    add localhost to internal ips

--- hello/hello/settings.py
+++ hello/hello/settings.py
@@ -15,6 +15,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
